2e1be1de8110e67657cc3bd39213b562059b0279
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-26 14:32:37 +1300

    Add static data.

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthdays today</h3>
+        <List />
+        <button className="btn" onClick={() => console.log('Ouch!')}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
