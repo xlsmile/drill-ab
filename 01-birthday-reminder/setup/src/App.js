7db0807c65fa9b824c23bf08bde476d2691bde25
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-26 15:07:02 +1300

    Add data to birthday reminder.

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 
-function App() {
+const App = () => {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List />
-        <button className="btn" onClick={() => console.log('Ouch!')}>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button className="btn" onClick={() => setPeople([])}>
           clear all
         </button>
       </section>
     </main>
   );
-}
+};
 
 export default App;
