f861dbfb51e8dd9da3370ed908e3ad89cb661d62
Serge <protomix@gmail.com>
2023-02-11 11:46:09 +1300

    Update Scss

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import Review from './Review';
 
 function App() {
-  return <Review />;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
 }
 
 export default App;
